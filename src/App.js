fe1a95065f50c16d80ef8b8fe736bedba7fdb4b0
Robert Pliszka <robplisz@gmail.com>
2023-04-24 19:01:10 -0400

    Added expense items to the page in an object array

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-
-
 function App() {
+  const expenses = [
+    {
+      id: "1",
+      title: "Rent",
+      amount: 1250.0,
+      date: new Date(2023, 7, 14),
+    },
+    {
+      id: "2",
+      title: "Phone bill",
+      amount: 125.0,
+      date: new Date(2023, 7, 18),
+    },
+    {
+      id: "3",
+      title: "Grocaries",
+      amount: 200.0,
+      date: new Date(2023, 7, 20),
+    },
+  ];
   return (
     <div>
-      <h1>Hello</h1>
+      <h1>Expense Item's</h1>
     </div>
   );
 }
