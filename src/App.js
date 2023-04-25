f7e2723b8433e892e5d9dc395c5d980a06427d42
Robert Pliszka <robplisz@gmail.com>
2023-04-24 20:39:02 -0400

    Finisehd the expense item component and added date and all to it. Tested all works

--- src/App.js
+++ src/App.js
@@ -6,19 +6,19 @@ function App() {
       id: "1",
       title: "Rent",
       amount: 1250.0,
-      date: new Date(2023, 7, 14),
+      date: new Date(2023, 2, 14),
     },
     {
       id: "2",
       title: "Phone bill",
       amount: 125.0,
-      date: new Date(2023, 7, 18),
+      date: new Date(2023, 2, 18),
     },
     {
       id: "3",
       title: "Grocaries",
       amount: 200.0,
-      date: new Date(2023, 7, 20),
+      date: new Date(2023, 2, 20),
     },
   ];
   return (
@@ -27,7 +27,17 @@ function App() {
       <ExpenseItem
         title={expenses[0].title}
         amount={expenses[0].amount}
-        date={expenses[0].data}
+        date={expenses[0].date}
+      />
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      />
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
       />
     </div>
   );
